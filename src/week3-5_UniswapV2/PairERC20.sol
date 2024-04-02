016cddf103443a4e436f56c4b21a958c62c84fea
Surbhi Audichya <audichyasurbhi@gmail.com>
2024-04-03 01:34:55 +0530

    refactor: Add the constant attribute to PairERC20 state variables that never change.

--- src/week3-5_UniswapV2/PairERC20.sol
+++ src/week3-5_UniswapV2/PairERC20.sol
@@ -4,8 +4,8 @@ pragma solidity 0.8.21;
 import {ERC20} from "solady/src/tokens/ERC20.sol";
 
 contract PairERC20 is ERC20 {
-    string internal _name = "Pair Liquidty Token";
-    string internal _symbol = "PLT";
+    string internal constant _name = "Pair Liquidity Token";
+    string internal constant _symbol = "PLT";
 
     function name() public view override returns (string memory) {
         return _name;
